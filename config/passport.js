7e3f849ef02cb32d68a5b0912ec786920f3643b7
Satvik Agrawal <Satvikagrawal007@gmail.com>
2026-02-22 23:17:54 +0530

    changes

--- config/passport.js
+++ config/passport.js
@@ -3,8 +3,8 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const User = require('../models/user');
 
 passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    clientID: process.env.GOOGLE_CLIENT_ID || 'missing_client_id',
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET || 'missing_client_secret',
     callbackURL: process.env.GOOGLE_CALLBACK_URL || "http://localhost:8001/auth/google/callback"
 },
     async function (accessToken, refreshToken, profile, cb) {
